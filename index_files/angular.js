3bf37bb826ce023208363ec146d820b42bcc0b3a
Vojta Jina <vojta.jina@gmail.com>
2012-04-11 11:47:32 -0700

    concept

---
+++ index_files/angular.js
@@ -0,0 +1,47 @@
+callback({
+  "data": {
+    "mirror_url": null,
+    "has_wiki": true,
+    "language": "JavaScript",
+    "ssh_url": "git@github.com:angular/angular.js.git",
+    "owner": {
+      "url": "https://api.github.com/users/angular",
+      "avatar_url": "https://secure.gravatar.com/avatar/6e1b5ab3ef1593413f1bee4e5a6e6ae7?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-orgs.png",
+      "login": "angular",
+      "gravatar_id": "6e1b5ab3ef1593413f1bee4e5a6e6ae7",
+      "id": 139426
+    },
+    "organization": {
+      "url": "https://api.github.com/users/angular",
+      "avatar_url": "https://secure.gravatar.com/avatar/6e1b5ab3ef1593413f1bee4e5a6e6ae7?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-orgs.png",
+      "login": "angular",
+      "gravatar_id": "6e1b5ab3ef1593413f1bee4e5a6e6ae7",
+      "id": 139426
+    },
+    "open_issues": 180,
+    "watchers": 469,
+    "pushed_at": "2012-04-10T23:52:17Z",
+    "url": "https://api.github.com/repos/angular/angular.js",
+    "description": "",
+    "svn_url": "https://github.com/angular/angular.js",
+    "updated_at": "2012-04-10T23:52:18Z",
+    "has_downloads": true,
+    "size": 1328,
+    "fork": false,
+    "has_issues": true,
+    "git_url": "git://github.com/angular/angular.js.git",
+    "name": "angular.js",
+    "forks": 96,
+    "homepage": "http://angularjs.org",
+    "clone_url": "https://github.com/angular/angular.js.git",
+    "html_url": "https://github.com/angular/angular.js",
+    "private": false,
+    "id": 460078,
+    "created_at": "2010-01-06T00:34:37Z"
+  },
+  "meta": {
+    "status": 200,
+    "X-RateLimit-Remaining": "4995",
+    "X-RateLimit-Limit": "5000"
+  }
+})
